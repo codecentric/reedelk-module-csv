8960457775ebebdf37ff26d1a2720234c058dc35
lorenzo <lorenzo@lorenzo.info.com>
2020-04-14 11:03:28 +0200

    code

--- src/test/java/com/reedelk/csv/component/CSVReadTest.java
+++ src/test/java/com/reedelk/csv/component/CSVReadTest.java
@@ -1,23 +1,32 @@
 package com.reedelk.csv.component;
 
+import com.reedelk.runtime.api.commons.ModuleContext;
 import com.reedelk.runtime.api.converter.ConverterService;
 import com.reedelk.runtime.api.flow.FlowContext;
 import com.reedelk.runtime.api.message.Message;
 import com.reedelk.runtime.api.message.MessageBuilder;
 import com.reedelk.runtime.api.message.content.DataRow;
 import com.reedelk.runtime.api.message.content.MimeType;
+import com.reedelk.runtime.api.script.ScriptEngineService;
+import com.reedelk.runtime.api.script.dynamicvalue.DynamicString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doAnswer;
+import static org.assertj.core.api.Assertions.in;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CSVReadTest {
@@ -29,6 +38,8 @@ class CSVReadTest {
     private FlowContext context;
     @Mock
     private ConverterService converter;
+    @Mock
+    private ScriptEngineService scriptService;
 
     private CSVRead csvRead = new CSVRead();
 
@@ -36,8 +47,9 @@ class CSVReadTest {
     void setUp() {
         csvRead = new CSVRead();
         csvRead.converter = converter;
+        csvRead.scriptService = scriptService;
 
-        doAnswer(invocation -> invocation.getArgument(0))
+        lenient().doAnswer(invocation -> invocation.getArgument(0))
                 .when(converter)
                 .convert(any(Object.class), any(Class.class));
     }
@@ -209,6 +221,49 @@ class CSVReadTest {
                 asList("Bud Grimsby","Center Field","\"The Reaper\", \"Longneck\"","1910-1917"));
         assertExistRecord(records,
                 asList("Vic Crumb","Shortstop","\"Fat Vic\", \"Icy Hot\"","1911-1912"));
+    }
+
+    @Test
+    void shouldReadCSVFromFile() throws IOException {
+        // Given
+        String csvData = CSVs.SAMPLE_WITH_HEADER.string();
+        Path tmpFilePath = createTmpFileWithData(csvData);
+        DynamicString dynamicFile = DynamicString.from("#['" + tmpFilePath.toString() + "']", new ModuleContext(10L));
+
+        csvRead.setFirstRecordAsHeader(true);
+        csvRead.setFile(dynamicFile);
+        csvRead.initialize();
+
+        Message input = MessageBuilder.get()
+                .empty()
+                .build();
+
+        doAnswer(invocation -> Optional.of(tmpFilePath.toString()))
+                .when(scriptService)
+                .evaluate(dynamicFile, context, input);
+
+        // When
+        Message actual = csvRead.apply(context, input);
+
+        // Then
+        List<DataRow<String>> records = actual.payload();
+
+        assertThat(records).hasSize(3);
+
+        assertExistRecord(records,
+                asList("Skippy Peterson","First Base","\"Blue Dog\", \"The Magician\"","1908-1913"));
+        assertExistRecord(records,
+                asList("Bud Grimsby","Center Field","\"The Reaper\", \"Longneck\"","1910-1917"));
+        assertExistRecord(records,
+                asList("Vic Crumb","Shortstop","\"Fat Vic\", \"Icy Hot\"","1911-1912"));
+    }
+
+    private Path createTmpFileWithData(String csvData) throws IOException {
+        String tmpFileName = UUID.randomUUID().toString() + ".csv";
+        Path tmpFilePath = Paths.get(System.getProperty("java.io.tmpdir"), tmpFileName);
+
+        Files.write(tmpFilePath, csvData.getBytes());
+        return tmpFilePath;
     }
 
     private void assertExistRecord(List<DataRow<String>> records, List<String> headers, List<String> expected) {
